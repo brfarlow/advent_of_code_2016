100f7774a4b669f564208c90c3b6f59193b65fc1
Branden <xorfos@gmail.com>
2017-08-25 08:10:58 -0400

    Day 1 solution for Advent of Code 2016

---
+++ day1/day1_p1.py
@@ -0,0 +1,55 @@
+class Taxicab:
+	def __init__(self):
+		self.x = 0
+		self.y = 0
+		self.direction = "n"
+
+	def turn_left(self):
+		if self.direction == "n":
+			self.direction = "w"
+		elif self.direction  =="w":
+			self.direction = "s"
+		elif self.direction == "s":
+			self.direction = "e"
+		else:
+			self.direction = "n"
+
+	def turn_right(self):
+		if self.direction == "n":
+			self.direction = "e"
+		elif self.direction == "w":
+			self.direction = "n"
+		elif self.direction == "s":
+			self.direction = "w"
+		elif self.direction == "e":
+			self.direction = "s"
+
+	def move(self):
+		if self.direction == "n":
+			self.y += 1
+		elif self.direction == "w":
+			self.x -= 1
+		elif self.direction == "s":
+			self.y -= 1
+		else:
+			self.x += 1
+
+
+if __name__ == '__main__':
+
+	with open('input.txt', 'r') as f:
+		content = f.read().replace(" ",'').split(",")
+	taxi = Taxicab()
+
+	for item in content:
+		if item[0] == "L":
+			taxi.turn_left()
+		else:
+			taxi.turn_right()
+
+		i = 0
+		while i < int(item[1:]):
+			i += 1
+			taxi.move()
+
+	print abs(taxi.x) + abs(taxi.y)
